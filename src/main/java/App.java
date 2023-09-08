fa596ec844e5ce204a18c47aec2ed46faa6987e1
illia-pastushenko <nzxt511@gmail.com>
2023-09-08 18:26:38 +0200

    code refactored

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,88 +3,156 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        byte input;
-        byte rand;
+
+        //initializing variable
+
         byte i;
-        boolean boxAvailable = false;
+        boolean boxAvailable; //initially was false
         byte winner = 0;
-        char box[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char[] box = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
         System.out.println("Enter box number to select. Enjoy!\n");
 
+
         boolean boxEmpty = false;
+
         while (true) {
-            System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
-            if(!boxEmpty){
-                for(i = 0; i < 9; i++)
-                    box[i] = ' ';
-                boxEmpty = true;
+
+            //printing the box
+            printBox(box);
+
+            //resettableIfNeeded
+            boxEmpty = resetBoxIfNeeded(box, boxEmpty);
+
+            //print "gameover" output if winner is 1,2 or 3
+            if (isGameResultDecided(winner)) break;
+
+            //move logic
+            moveLogic(box);
+
+
+            //check if user has won
+            if(isUserHasWon(box)) {
+                winner = 1;
+                continue;
             }
 
-            if(winner == 1){
-                System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            } else if(winner == 2){
-                System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            } else if(winner == 3){
-                System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            }
-
-            while (true) {
-                input = scan.nextByte();
-                if (input > 0 && input < 10) {
-                    if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
-                    else {
-                        box[input - 1] = 'X';
-                        break;
-                    }
-                }
-                else
-                    System.out.println("Invalid input. Enter again.");
-            }
-
-            if((box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[3]=='X' && box[6]=='X') || (box[1]=='X' && box[4]=='X' && box[7]=='X') || (box[2]=='X' && box[5]=='X' && box[8]=='X') ||
-               (box[0]=='X' && box[4]=='X' && box[8]=='X') || (box[2]=='X' && box[4]=='X' && box[6]=='X')){
-                   winner = 1;
-                   continue;
-            }
 
             boxAvailable = false;
-            for(i=0; i<9; i++){
-                if(box[i] != 'X' && box[i] != 'O'){
+
+            for (i = 0; i < 9; i++) {
+                if (box[i] != 'X' && box[i] != 'O') {
                     boxAvailable = true;
                     break;
                 }
             }
 
-            if(boxAvailable == false){
+            if (!boxAvailable) {
                 winner = 3;
                 continue;
             }
 
-            while (true) {
-                rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
-                    box[rand - 1] = 'O';
-                    break;
-                }
-            }
+            //computer move logic
+            computerMoveLogic(box);
 
-            if((box[0]=='O' && box[1]=='O' && box[2]=='O') || (box[3]=='O' && box[4]=='O' && box[5]=='O') || (box[6]=='O' && box[7]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[3]=='O' && box[6]=='O') || (box[1]=='O' && box[4]=='O' && box[7]=='O') || (box[2]=='O' && box[5]=='O' && box[8]=='O') ||
-               (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O')){
+            //check if computer has wom
+            if(isComputerHasWon(box)){
                 winner = 2;
-                continue;
             }
         }
 
     }
+
+    static void printBox(char[] box) {
+        System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
+        System.out.println("-----------");
+        System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
+        System.out.println("-----------");
+        System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
+    }
+
+    static boolean resetBoxIfNeeded(char[] box, boolean boxEmpty) {
+        if (!boxEmpty) {
+            for (int i = 0; i < 9; i++)
+                box[i] = ' ';
+        }
+        return true;
+    }
+
+    static boolean isGameResultDecided(byte winner) {
+        if (winner == 1) {
+            System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            return true;
+        } else if (winner == 2) {
+            System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            return true;
+        } else if (winner == 3) {
+            System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+            return true;
+        }
+        return false;
+    }
+
+    static void moveLogic(char[] box) {
+        Scanner scan = new Scanner(System.in);
+        byte input;
+        while (true) {
+            input = scan.nextByte();
+            if (input > 0 && input < 10) {
+                if (box[input - 1] == 'X' || box[input - 1] == 'O')
+                    System.out.println("That one is already in use. Enter another.");
+                else {
+                    box[input - 1] = 'X';
+                    break;
+                }
+            } else
+                System.out.println("Invalid input. Enter again.");
+        }
+    }
+
+    static boolean isUserHasWon(char[] box) {
+        return ((box[0] == 'X' && box[1] == 'X' && box[2] == 'X') || (box[3] == 'X' && box[4] == 'X' && box[5] == 'X') || (box[6] == 'X' && box[7] == 'X' && box[8] == 'X') ||
+                (box[0] == 'X' && box[3] == 'X' && box[6] == 'X') || (box[1] == 'X' && box[4] == 'X' && box[7] == 'X') || (box[2] == 'X' && box[5] == 'X' && box[8] == 'X') ||
+                (box[0] == 'X' && box[4] == 'X' && box[8] == 'X') || (box[2] == 'X' && box[4] == 'X' && box[6] == 'X'));
+    }
+    static boolean isComputerHasWon(char[] box){
+        return((box[0] == 'O' && box[1] == 'O' && box[2] == 'O') || (box[3] == 'O' && box[4] == 'O' && box[5] == 'O') || (box[6] == 'O' && box[7] == 'O' && box[8] == 'O') ||
+                (box[0] == 'O' && box[3] == 'O' && box[6] == 'O') || (box[1] == 'O' && box[4] == 'O' && box[7] == 'O') || (box[2] == 'O' && box[5] == 'O' && box[8] == 'O') ||
+                (box[0] == 'O' && box[4] == 'O' && box[8] == 'O') || (box[2] == 'O' && box[4] == 'O' && box[6] == 'O'));
+
+    }
+    static void computerMoveLogic(char[] box){
+        byte rand;
+        while (true) {
+            rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
+            if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
+                box[rand - 1] = 'O';
+                break;
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
